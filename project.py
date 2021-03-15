fd67e51bce97852cfd266fc0eb2afdd2270fb00d
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2021-03-16 00:15:34 +0800

    Implement method that will return all the completed projects

--- project.py
+++ project.py
@@ -2,20 +2,11 @@ import csv
 
 class Project:
 
-    def __init__(self, id, title, size, priority):
-        self.id = id
+    def __init__(self, project_id, title, size, priority):
+        self.id = project_id
         self.title = title
         self.size = size
         self.priority = priority
-    
-    def getTitle(self):
-        return title
-    
-    def getSize(self):
-        return size
-    
-    def getPriority(self):
-        return priority
 
     def getOneProject(self, project_id):
         with open('projects.txt', 'r') as csv_file:
@@ -25,9 +16,20 @@ class Project:
                 if(row[0] == project_id):
                     print(row[0])
     
+    def getCompletedProjects(self):
+        project_list = []
+
+        with open('completed_projects.txt', 'r') as csv_file:
+            projects = csv.reader(csv_file)
+            next(projects)
+            for row in projects:
+                project_list.append(row)
+        
+        print(project_list)
+
     def getAllProjects(self):
         project_list = []
-        
+
         with open('projects.txt', 'r') as csv_file:
             projects = csv.reader(csv_file)
             next(projects)
