ab93557c1dcce0455f99b18d392b02e5ebed7d88
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2021-03-16 00:52:14 +0800

    Implement additional methods

--- project_controller.py
+++ project_controller.py
@@ -1,8 +1,10 @@
-class ProjectController:   
-    
-    # Implement as Singleton
+import csv
 
-    def getOneProject(project_id):
+class ProjectController:   
+    project_queue = [['69', 'Title 69', '69', '69']]
+
+    # View Projects methods
+    def getOneProject(self, project_id):
         with open('projects.txt', 'r') as csv_file:
             projects = csv.reader(csv_file)
             next(projects)
@@ -10,27 +12,43 @@ class ProjectController:
                 if(row[0] == project_id):
                     print(row[0])
     
-    def getCompletedProjects():
-        project_list = []
+    def getCompletedProjects(self):
+        completed_projects = []
 
         with open('completed_projects.txt', 'r') as csv_file:
             projects = csv.reader(csv_file)
             next(projects)
             for row in projects:
-                project_list.append(row)
+                completed_projects.append(row)
         
-        print(project_list)
+        print(completed_projects)
 
-    def getAllProjects():
-        project_list = []
+    def getAllProjects(self):
+        all_projects = []
 
         with open('projects.txt', 'r') as csv_file:
             projects = csv.reader(csv_file)
             next(projects)
             for row in projects:
-                project_list.append(row)
+                all_projects.append(row)
         
-        print(project_list)
+        print(all_projects)
 
-    def getAProject():
-        # TODO
+    # Schedule Projects methods
+    def createSchedule(self):
+        with open('projects.txt', 'r') as csv_file:
+            projects = csv.reader(csv_file)
+            next(projects)
+            for row in projects:
+                ProjectController.project_queue.append(row)
+            
+
+    def viewUpdatedSchedule(self):
+
+
+    # Get a Project method
+    def getAProject(self):
+        print('Project removed from the queue.')
+        with open('completed_projects.txt', 'a') as completed_projects:
+            project = ','.join(ProjectController.project_queue.pop(0))
+            completed_projects.write(project + '\n')
