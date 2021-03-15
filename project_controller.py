6a328698e462da2a32c98889a275350bbbddcbb9
Ryou Hikaru <carljamesreblora13@gmail.com>
2021-03-16 00:59:11 +0800

    Singleton enabled

--- project_controller.py
+++ project_controller.py
@@ -2,6 +2,13 @@ import csv
 
 class ProjectController:   
     project_queue = [['69', 'Title 69', '69', '69']]
+    __instance = None
+
+    # Implemented as Singleton
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(ProjectController, cls).__new__(cls)
+        return cls.__instance
 
     # View Projects methods
     def getOneProject(self, project_id):
@@ -44,7 +51,7 @@ class ProjectController:
             
 
     def viewUpdatedSchedule(self):
-
+        pass
 
     # Get a Project method
     def getAProject(self):
