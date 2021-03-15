b6ea675db12abba50dd1dc13c7bb7ae6e9747b9a
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2021-03-16 00:26:44 +0800

    Move methods to the controller class

--- project.py
+++ project.py
@@ -1,39 +1,8 @@
 import csv
 
 class Project:
-
     def __init__(self, project_id, title, size, priority):
         self.id = project_id
         self.title = title
         self.size = size
         self.priority = priority
-
-    def getOneProject(self, project_id):
-        with open('projects.txt', 'r') as csv_file:
-            projects = csv.reader(csv_file)
-            next(projects)
-            for row in projects:
-                if(row[0] == project_id):
-                    print(row[0])
-    
-    def getCompletedProjects(self):
-        project_list = []
-
-        with open('completed_projects.txt', 'r') as csv_file:
-            projects = csv.reader(csv_file)
-            next(projects)
-            for row in projects:
-                project_list.append(row)
-        
-        print(project_list)
-
-    def getAllProjects(self):
-        project_list = []
-
-        with open('projects.txt', 'r') as csv_file:
-            projects = csv.reader(csv_file)
-            next(projects)
-            for row in projects:
-                project_list.append(row)
-        
-        print(project_list)
