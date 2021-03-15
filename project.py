d04599274aae57f2b19d49ea10b6323a6dc32930
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2021-03-15 22:42:23 +0800

    Create model class that will represent a project

---
+++ project.py
@@ -0,0 +1,27 @@
+class Project:
+    def __init__(self, id, title, size, priority):
+        self.id = id
+        self.title = title
+        self.size = size
+        self.priority = priority
+
+    def getId():
+        return id
+    
+    def getTitle():
+        return title
+    
+    def getSize():
+        return size
+    
+    def getPriority():
+        return priority
+
+    def setTitle(title):
+        self.title = title
+
+    def setSize(size):
+        self.size = size
+    
+    def setPriority(priority):
+        self.priority = priority
