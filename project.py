c0d77ced7506465570fc019db2953a3ce25909fa
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2021-03-21 16:35:02 +0800

    Remove project.py since it was not used

--- project.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-class Project:
-    def __init__(self, project_id, title, size, priority):
-        self.id = project_id
-        self.title = title
-        self.size = size
-        self.priority = priority
